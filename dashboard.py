e162c456cf2708567c15b4d7b7eb85600d1e1370
Brandon Huang <bhuang23@gmail.com>
2022-07-27 00:01:38 -0400

    .

---
+++ dashboard.py
@@ -0,0 +1,43 @@
+import panel as pn
+from panel.interact import interact, interactive, fixed, interact_manual
+from panel import widgets
+import pandas as pd
+import numpy as np
+import tickers_graphing_module as tg
+import analysis_module as am
+import analytics_functions as af
+import plotly.graph_objects as go
+import plotly.express as px
+import matplotlib.pyplot as plt
+import importlib
+importlib.reload(am)
+importlib.reload(tg)
+importlib.reload(af)
+
+
+pn.extension()
+
+button = pn.widgets.Button(name='Get Ticker Stats and Financials', button_type='primary')
+text = pn.widgets.TextInput(name = 'Ticker', value='TSLA')
+def b(event):
+    af.get_share_stats_and_financials(text.value)
+
+
+button2 = pn.widgets.Button(name='Swaggy Stocks', button_type='primary')
+def c(event):
+    af.swaggy_stocks()
+
+
+button3 = pn.widgets.Button(name='Get Options Chain', button_type='primary')
+text3 = pn.widgets.TextInput(name = 'Options Chain Ticker', value='TSLA')
+def d(event):
+    af.get_options_chain(text3.value)
+
+
+button.on_click(b)
+button2.on_click(c)
+button3.on_click(d)
+dash = pn.Column(text, button, pn.Spacer(height = 5), button2, pn.Spacer(height = 5), text3, button3)
+
+
+dash.show()
